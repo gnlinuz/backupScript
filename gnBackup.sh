5ccd3e27b8f4f09d22bc27655834eb37a77ee7dd
George Nikolaidis <gnlinuz@yahoo.com>
2017-10-27 18:52:55 +0300

    program development

--- gnBackup.sh
+++ gnBackup.sh
@@ -14,6 +14,33 @@
 TIME=`date +%Y%m%d-%H%M%S`
 DN=F                                      
 IN=I
+LISENCE="You can freely use/redistribute gnBackup, to anyone as
+long as you want, under the terms  of  the GNU General
+Public License  as   published by  the  Free  Software
+Foundation or modify it as  long as you do  not  alter
+this notice or the authors information. You can change
+the original code, definitely improve it so to  become
+a co-author of the script,  just  inform  all  of  the
+authors and send  them  your  improved  script  so  to
+maintain the version of it. Therefore add your contact
+information and state your modifications so anyone has
+the opportunity to contact the authors for bugs or so.
+If you are just a user of it,
+USE THIS SCRIPT AT YOUR OWN RISK !!  Authors  are  not
+responsible for any  missuse  or any damage that might
+happen. Have fun backing up!!\n"
+
+RestoreData="to Restore...
+goto your backup directory, find the file you want
+to Restore and type the following command...
+example:
+cat I-20171018-195601.tar.gz_* | tar xzvf -
+----------------------------------------------------------------
+if you want to Restore it into a different directory
+other than current, type the following command...
+example:
+cat I-20171018-195601.tar.gz_* | tar xzvf - -C /target/directory\n"
+
 #FILENAME=dataBackup-$TIME.tar.gz                                
 #DESDIR=/dataBackup                                                  
 #cat F.backup.tar.gz_* | tar xzvf -
@@ -29,27 +56,29 @@ start()
 {
 clear
 echo
-echo -e '\E[36;40m'"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "              Script to backup your files or folders"
+#echo -e '\E[36;40m'"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"; echo -e '\E[0m'
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo " Backup script, splits large files into multiple chuncks of"
-echo " 2GB files to be easilly managed by most media"
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"; echo -e '\E[0m'
-for i in {1..3}
-do
-	sleep 1
-	echo -n .
-done
-clear
+echo "                  Backup your files or folders"
+#echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+#echo " Backup script, splits large files into multiple chuncks of"
+#echo " 2GB files to be easilly managed by most media"
+#echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"; echo -e '\E[0m'
+#for i in {1..3}
+#do
+#	sleep 1
+#	echo -n .
+#done
+#clear
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo " 1. Run a full backup now"
 echo " 2. Schedule a backup (Full/Incremental)"
-echo " 3. Restore instatructions"
-echo " 4. Change default 2GB backup size"
-echo " 5. Check for schedule"
-echo " 6. About"
-echo " 7. Exit."
+echo " 3. Dump/Backup MySQL/MariaDB"
+echo " 4. Restore instatructions"
+echo " 5. Change default 2GB backup size"
+echo " 6. Check for schedule"
+echo " 7. About the script"
+echo " 8. Exit."
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo -e '\E[36;40m'" NOTE: keep in mind that  script  uses  crontab  for schedulling"
@@ -61,9 +90,6 @@ tput civis
 #tput cnorm cursor is on again
 read -n 1 -s selection;
 }
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #                                          select multiple source file or folders
@@ -104,13 +130,9 @@ printf "your selection is: "
 echo -en '\E[36;40m'"$mSrcPath"; echo -e '\E[0m'
 echo "-------------------------------------------------------------------"
 }
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                               set source, destination, name, destination folder check 
+#                                         set destination, destination check 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 jobNow()
 {
@@ -138,8 +160,6 @@ do
 	fi
 done
 }
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #                                                 set which program to follow
@@ -272,6 +292,135 @@ else
 fi
 }
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                                      MySQL MariaDB backup
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sqlBackup()
+{
+	echo
+	echo "-------------------------------------------------------------------"
+	echo " 1. dump/backup all databases"
+	echo " 2. dump/backup specific database"
+	echo " 3. exit"
+	echo "-------------------------------------------------------------------"
+	read -n 1 -s sqlSel;
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                                      Backup all databases
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+allSql()
+{
+	tput cnorm
+	echo "-------------------------------------------------------------------"
+	printf "Enter mySQL root user or any other superUser: "
+	read -r sUser
+	(mysqldump -u$sUser -p --all-databases | gzip > $dest_path/allDatabases$TIME.sql.gz) >/var/log/myBackup.log 2>&1
+	if [ $? -eq 0 ];then
+                echo "-------------------------------------------------------------------"
+                echo -en '\E[36;40m'"Backup finished successfull...check the backup folder"; echo -e '\E[0m'
+                echo -en '\E[36;40m'"log is available at /var/log/myBackup.log"; echo -e '\E[0m'
+                echo "-------------------------------------------------------------------"
+        else
+                echo "-------------------------------------------------------------------"
+                echo -en '\E[31;40m'"the backup did not executed properly, exit status is: $?"; echo -e '\E[0m'
+                echo -en '\E[31;40m'"check the log file at /var/log/myBackup.log"; echo -e '\E[0m'
+                echo "-------------------------------------------------------------------"
+        fi
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                                      Backup one datatbase
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+oneDB()
+{
+	tput cnorm
+	echo "-------------------------------------------------------------------"
+	printf "Enter mySQL root user or any other superUser: "
+	read -r sUser
+	printf "Enter valid database name: "
+	read -r dbName
+	(mysqldump -u$sUser -p $dbName |gzip > $dest_path/$dbName$TIME.sql.gz) >/var/log/myBackup.log 2>&1
+	if [ $? -eq 0 ];then
+                echo "-------------------------------------------------------------------"
+                echo -en '\E[36;40m'"Backup finished successfull...check the backup folder"; echo -e '\E[0m'
+                echo -en '\E[36;40m'"log is available at /var/log/myBackup.log"; echo -e '\E[0m'
+                echo "-------------------------------------------------------------------"
+        else
+                echo "-------------------------------------------------------------------"
+                echo -en '\E[31;40m'"the backup did not executed properly, exit status is: $?"; echo -e '\E[0m'
+                echo -en '\E[31;40m'"check the log file at /var/log/myBackup.log"; echo -e '\E[0m'
+                echo "-------------------------------------------------------------------"
+        fi
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                                      Restore Menu
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+restoreSel()
+{
+	clear
+	echo
+	echo "-------------------------------------------------------------------"
+	echo " 1. Restore data"
+	echo " 2. Restore all-databases MySQL/MariaDB"
+	echo " 3. Restore one database MySQL/MariaDB"
+	echo " 4. Exit"
+	echo "-------------------------------------------------------------------"
+	read -n 1 -s resSel;
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                                      Restore all databases
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+restoreAll()
+{
+	clear
+	echo -e '\E[36;40m'"-------------------------------------------------------------------"
+	echo "example:  /dataBackup/allDatabases20171027-171744.sql.gz"; echo -e '\E[0m'
+	printf "Please enter the path and database backup name: "
+        read -r dbName
+	printf "Enter mySQL root user or any other superUser: "
+        read -r sUser
+	gunzip < $dbName | mysql -u$sUser -p
+	if [ $? -eq 0 ];then
+                echo "-------------------------------------------------------------------"
+                echo -en '\E[36;40m'"Restore finished successfull..."; echo -e '\E[0m'
+                echo "-------------------------------------------------------------------"
+        else
+                echo "-------------------------------------------------------------------"
+                echo -en '\E[31;40m'"Restore did not executed properly, exit status is: $?"; echo -e '\E[0m'
+                echo "-------------------------------------------------------------------"
+        fi
+	
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                                      Restore one datatbase
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+restoreOne()
+{
+	clear
+	echo -e '\E[36;40m'"-------------------------------------------------------------------"
+        echo "example:  /dataBackup/allDatabases20171027-171744.sql.gz"; echo -e '\E[0m'
+        printf "Please enter the path and database backup name: "
+        read -r dbName
+        printf "Enter mySQL root user or any other superUser: "
+        read -r sUser
+	printf "Enter the database name that will be replaced: "
+	read -r dbN
+        gunzip < $dbName | mysql -u$sUser -p $dbN
+	if [ $? -eq 0 ];then
+                echo "-------------------------------------------------------------------"
+                echo -en '\E[36;40m'"Restore finished successfull..."; echo -e '\E[0m'
+                echo "-------------------------------------------------------------------"
+        else
+                echo "-------------------------------------------------------------------"
+                echo -en '\E[31;40m'"Restore did not executed properly, exit status is: $?"; echo -e '\E[0m'
+                echo "-------------------------------------------------------------------"
+        fi
+}
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #                                                 selection case
@@ -296,23 +445,41 @@ case $selection in
 		schedule
 	;;
 	3)	clear
-		echo -e '\E[36;40m'"-------------------------------------------------------------------"
-		echo " to Restore..." 
-		echo " goto your backup directory, find the file you want"
-		echo " to Restore and type the following command..." 
-		echo " example:"
-		echo " cat I-20171018-195601.tar.gz_* | tar xzvf -"
-		echo
-		echo " if you want to Restore it into a different directory"
-		echo " other than current, type the following command..."
-		echo " example:"
-		echo " cat I-20171018-195601.tar.gz_* | tar xzvf - -C /target/directory"
-		echo "-------------------------------------------------------------------"; echo -e '\E[0m'
+		sqlBackup
+		if [ $sqlSel -eq 1 ]; then 
+			jobNow
+			allSql 
+		fi
+		if [ $sqlSel -eq 2 ]; then 
+			jobNow
+			oneDB 
+		fi
+		if [ $sqlSel -eq 3 ]; then
+                	exit
+                fi
 	;;
-	5)	clear
-		crontab -l
+	4)	restoreSel
+		clear
+		if [ $resSel -eq 1 ]; then
+                        echo -e '\E[36;40m'"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+			printf "$RestoreData"
+			echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"; echo -e '\E[0m'
+                fi
+		if [ $resSel -eq 2 ]; then
+                        restoreAll
+                fi
+		if [ $resSel -eq 3 ]; then
+                        restoreOne
+                fi
+		if [ $resSel -eq 4 ]; then
+                        exit
+                fi
 	;;
 	6)	clear
+		echo
+		crontab -l
+	;;
+	7)	clear
 		echo -e '\E[36;40m'"+++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 		echo " Purpose = (Full | Incremental) Backup's"
 		echo " Created on 10/10/2017"
@@ -325,28 +492,14 @@ case $selection in
 		echo " Your modification:"
 		echo " Conatct = "
 		echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-		echo " You can freely use it, or  redistribute  it  to anyone" 
-		echo " as long as you want,  or  modify it under the terms of" 
-		echo " the GNU General Public  License  as  published by  the"
-		echo " Free Software Foundation as  long as  you do not alter" 
-		echo " this notice or the authors information. You can change"
-		echo " the original code, definitely improve it so to  become"
-		echo " a co-author of the script,  just  inform  all  of  the" 
-		echo " authors and send  them  your  improved  script  so  to"
-		echo " maintain the version of it. Therefore add your contact"
-		echo " information and state your modifications so anyone has"
-		echo " the opportunity to contact the authors for bugs or so."
-		echo " If you are just a user of it," 
-		echo " USE THIS SCRIPT AT YOUR OWN RISK !!  Authors  are  not" 
-		echo " responsible for any  missuse  or any damage that might" 
-		echo " happen. Have fun backing up!!"
+		printf "$LISENCE"
 		echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++"; echo -e '\E[0m'
 	;;
-	7)	echo " sagionara..."
+	8)	echo " sagionara..."
 		tput cnorm
 		exit 0
 	;;
-	*) echo " choose: {1 | 2 | 3 | 4 | 5 | 6} "
+	*) echo " choose: {1|2|3|4|5|6|7|8} "
 	   exit 1
 	;;
 esac
